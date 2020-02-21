15b7268c78040e422792ccaf00b3eba15152dd35
Amir Masoud Abdol <amirmasoudabdol@icloud.com>
2020-02-21 17:56:39 +0100

    Added some of the common template files to the OO. I'm thinking to incorporate these to the Makefile too but I don't know how yet

---
+++ templates/marjan_2012_small_post_processing.R
@@ -0,0 +1,68 @@
+# Customize this file!
+# The only reaason that this file exists is that RStudio has an
+# issue with multiprocess plan and apparently it's not stable,
+# so, I'm leaving it out to run it in parallel via the Makefile
+
+suppressWarnings(suppressMessages(library(tidyverse)))
+suppressWarnings(suppressMessages(library(furrr)))
+suppressWarnings(suppressMessages(library(data.table)))
+
+plan(multiprocess)
+
+project_path <- "outputs/"
+project_name <- "marjan_2012_small"
+
+# Currently I'm mainly processing prepared publications datasets as
+# they contain most of the data that I need, but you can apply this
+# script on other outputs as well, e.g., `stats`.
+filenames <- list.files(project_path, pattern = "*_pubs_prepared.csv", full.names = TRUE)
+
+# Reading and summarizing each file
+summarize_each_file <- function(fname) {
+
+  df <- fread(fname)
+
+  # You can use this snippet as an example of how to summarize your data for
+  # later post-processing and plotting.
+  df %>% 
+    # filter(effect > 0) %>%
+    mutate(tnobs = factor(tnobs),
+           nobs = nobs,
+           covs = factor(experiment_parameters_data_strategy_measurements_covs),
+           decision_strategy = factor(researcher_parameters_decision_strategy__name),
+           is_hacked = factor(researcher_parameters_is_phacker),
+           selection_pref = factor(researcher_parameters_decision_strategy_preference),
+           tmean = experiment_parameters_data_strategy_measurements_means_5,
+           effect = effect) %>%
+    mutate(eff_abs_diff= effect - tmean) %>%
+    group_by(tmean, tnobs, covs, decision_strategy, is_hacked, selection_pref) %>%
+    summarize(sigmean = mean(sig[effect > 0.0]),
+              mean_nobs = mean(nobs),
+              mean_eff = mean(effect),
+              mean_eff_diff = mean(eff_abs_diff)) %>%
+              mutate(size = "Small") -> agg_df
+  
+  return(agg_df)
+}
+
+
+# Reading and summarizing all files in parallel
+read_all_files <- function(fnames) {
+
+  options(warn =  -1)
+  
+  tbl <-
+    filenames %>%
+    future_map_dfr(~summarize_each_file(.), .progress = FALSE)
+
+  return(tbl)
+
+}
+
+df <- read_all_files(filenames)
+
+# Removig the grouping columns
+df <- data.frame(df)
+
+
+write.csv(df, paste(project_path, project_name, "_summarized_df.csv", sep=""), row.names = FALSE)
