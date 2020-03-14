d58ed47f42de23f500e77e277663522c86b114d3
Amir Masoud Abdol <amirmasoudabdol@icloud.com>
2020-03-14 13:42:45 +0100

    added more cases to small, and large studies

--- templates/marjan_2012_small_prepare_config_files.py
+++ templates/marjan_2012_small_prepare_config_files.py
@@ -41,8 +41,7 @@ params_info = {
 	"journal_max_pubs": [10000],
 
 	"decision_strategy_name": ["ImpatientDecisionMaker"],
-	"decision_strategy_policies": [[["first"]],
-									 [["random"]]]
+	"decision_strategy_policies": [[["first"]], [["random"]], [["min(pvalue)"]], [["max(pvalue)"]]]
 	}
 
 
