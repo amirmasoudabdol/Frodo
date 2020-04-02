6badfc8e4580abf1cddfa6c4ea55bd63380d5e54
Amir Masoud Abdol <amirmasoudabdol@icloud.com>
2020-04-02 15:51:37 +0200

    These are correct configurations for getting Marjan's 2012 paper right!

---
+++ templates/marjan_2012_no_qrp_post_processing.R
@@ -0,0 +1,80 @@
+# Customize this file!
+# The only reaason that this file exists is that RStudio has an
+# issue with multiprocess plan and apparently it's not stable,
+# so, I'm leaving it out to run it in parallel via the Makefile
+
+suppressWarnings(suppressMessages(library(tidyverse)))
+suppressWarnings(suppressMessages(library(furrr)))
+suppressWarnings(suppressMessages(library(data.table)))
+
+plan(multiprocess)
+
+project_path <- "outputs/"
+project_name <- "marjan_2012_no_qrp"
+
+# Currently I'm mainly processing prepared publications datasets as
+# they contain most of the data that I need, but you can apply this
+# script on other outputs as well, e.g., `stats`.
+filenames <- list.files(project_path, pattern = "*_pubs_prepared.csv", full.names = TRUE)
+
+determine_size_class <- function(x) {
+  if (x %in% c(10, 50)) return("S")
+  if (x %in% c(20, 100)) return("M")
+  if (x %in% c(40, 200)) return("L")
+}
+
+determine_size <- function(x) {
+  if (x %in% c(10, 20, 40)) return("Small")
+  if (x %in% c(50, 100, 200)) return("Large")
+}
+
+# Reading and summarizing each file
+summarize_each_file <- function(fname) {
+
+  df <- fread(fname)
+  
+  # You can use this snippet as an example of how to summarize your data for
+  # later post-processing and plotting.
+  df %>% 
+    mutate(sizeclass = determine_size_class(as.numeric(tnobs))) %>%
+    mutate(size = determine_size(as.numeric(tnobs))) %>%
+    mutate(nobs = nobs,
+           tnobs = factor(tnobs),
+           covs = factor(experiment_parameters_data_strategy_measurements_covs),
+           decision_strategy = factor(researcher_parameters_decision_strategy__name),
+           is_hacked = factor(researcher_parameters_is_phacker),
+           selection_policy = factor(researcher_parameters_decision_strategy_initial_decision_policies_0_0),
+           tmean = experiment_parameters_data_strategy_measurements_means_2,
+           effect = effect) %>%
+    mutate(eff_abs_diff= effect - tmean) %>%
+    group_by(tmean, covs, tnobs, sizeclass, decision_strategy, is_hacked, selection_policy) %>%
+    summarize(sigmean = mean(sig),
+              mean_nobs = mean(nobs),
+              mean_eff = mean(effect),
+              mean_eff_diff = mean(eff_abs_diff),
+              size = head(size, 1)) -> agg_df
+  
+  return(agg_df)
+}
+
+
+# Reading and summarizing all files in parallel
+read_all_files <- function(fnames) {
+
+  options(warn =  -1)
+  
+  tbl <-
+    filenames %>%
+    future_map_dfr(~summarize_each_file(.), .progress = TRUE)
+
+  return(tbl)
+
+}
+
+df <- read_all_files(filenames)
+
+# Removig the grouping columns
+df <- data.frame(df)
+
+
+write.csv(df, paste(project_path, project_name, "_summarized_df.csv", sep=""), row.names = FALSE)
---
+++ templates/marjan_2012_no_qrp_prepare_config_files.py
@@ -0,0 +1,230 @@
+import json
+import uuid
+import itertools
+import numpy as np
+import tqdm
+
+params_info = {
+	"n_sims": [1],
+	"debug": [False],
+	"progress": [False],
+	"verbose": [False],
+	"data_strategy_n_conditions": [2],
+	"data_strategy_n_dep_vars": [2],
+	"data_strategy_measurements": [
+		{
+		"dist": "mvnorm_distribution",
+    	"means": [0.0, 0.0, x, x],
+        "covs": 0.5,
+        "stddevs": 1.0
+		} for x in np.arange(0.0, 1.01, 0.1)
+	],
+	"n_obs": [10, 20, 40, 50, 100, 200],
+	"k": [2],
+	"seed": ["random"],
+	"is_pre_processing": [False],
+	"is_phacker": [False],
+	"save_pubs": [True],
+	"save_sims": [False],
+	"save_stats": [False],
+	"save_rejected": [False],
+	"output_path": ["../outputs/"],
+	"output_prefix": [""],
+
+	"test_alpha": [0.05],
+	"test_strategy_name": ["TTest"],
+	"test_strategy_alternative": ["TwoSided"],
+
+	"effect_strategy_name": ["MeanDifference"],
+
+	"journal_selection_strategy_name": ["FreeSelection"],
+	"journal_max_pubs": [1000],
+
+	"decision_strategy_name": ["MarjansDecisionMaker"]
+	}
+
+
+def main():
+
+	configfilenames = open("configfilenames.pool", 'w')
+
+	counter = 0;
+	for param_vals in tqdm.tqdm(itertools.product(*params_info.values()), leave=False):
+
+		counter += 1
+
+		params = dict(zip(params_info.keys(), param_vals))
+
+		data = {
+			"experiment_parameters": {
+				    "data_strategy": {
+				        "_name": "LinearModel",
+				        "measurements": params["data_strategy_measurements"]
+				    },
+					"effect_strategy": {
+							"_name": params["effect_strategy_name"]
+					},
+					"n_conditions": params["data_strategy_n_conditions"],
+					"n_dep_vars": params["data_strategy_n_dep_vars"],
+					"n_obs": params["n_obs"],
+                    "n_reps": 1 if params["n_obs"] in [50, 100, 200] else 5,
+					"test_strategy": {
+							"_name": params["test_strategy_name"],
+							"alpha": params["test_alpha"],
+							"alternative": params["test_strategy_alternative"]
+					}
+			},
+			"journal_parameters": {
+					"max_pubs": params["journal_max_pubs"],
+					"selection_strategy": {
+							"_name": params["journal_selection_strategy_name"]
+					}
+			},
+			"researcher_parameters": {
+					"decision_strategy": {
+				      "_name": params["decision_strategy_name"],
+				      "between_replications_decision_policies": [
+			                [
+			                    "sig",
+			                    "effect > 0",
+			                    "first"
+			                ],
+			                [
+			                    "effect > 0",
+			                    "min(pvalue)"
+			                ],
+			                [
+			                    "effect < 0",
+			                    "max(pvalue)"
+			                ]
+			            ],
+				      "final_decision_policies": [
+				        [
+				          "sig",
+				          "effect > 0",
+				          "first"
+				        ],
+				        [
+				          "effect > 0",
+				          "min(pvalue)"
+				        ],
+				        [
+				          "effect < 0",
+				          "max(pvalue)"
+				        ]
+				      ],
+				      "initial_decision_policies": [
+				        [
+				          "id == 3",
+				          "sig",
+				          "effect > 0"
+				        ],
+				        [
+				          "id == 4",
+				          "sig",
+				          "effect > 0"
+				        ]
+				      ],
+				      "submission_policies": ["none"]
+				    },
+					"is_phacker": params["is_phacker"],
+				    "hacking_strategies": [
+				      [
+				        {
+				          "_name": "OptionalStopping",
+				          "level": "dv",
+				          "num": 10,
+				          "n_attempts": 1,
+				          "max_attempts": 1
+				        },
+				        [
+				          [
+				            "sig",
+				            "effect > 0",
+				            "first"
+				          ]
+				        ]
+				      ],
+				      [
+				        {
+				          "_name": "OptionalStopping",
+				          "level": "dv",
+				          "num": 10,
+				          "n_attempts": 1,
+				          "max_attempts": 1
+				        },
+				        {
+				          "_name": "OutliersRemoval",
+				          "level": "dv",
+				          "max_attempts": 1,
+				          "min_observations": 1,
+				          "mode": "Recursive",
+				          "multipliers": [
+				            2
+				          ],
+				          "n_attempts": 1,
+				          "num": 200,
+				          "order": "random"
+				        },
+				        [
+				          [
+				            "sig",
+				            "effect > 0",
+				            "first"
+				          ]
+				        ]
+				      ]
+				    ],
+					"is_pre_processing": params["is_pre_processing"],
+					"pre_processing_methods": [
+								{
+					               "_name": "OptionalStopping",
+					               "level": "dv",
+					               "num": 10,
+					               "n_attempts": 1,
+					               "max_attempts": 1
+					          	},
+								{
+									"_name": "OutliersRemoval",
+									"level": "dv",
+									"max_attempts": 1,
+									"min_observations": 1,
+									"mode": "Recursive",
+									"multipliers": [
+											2
+									],
+									"n_attempts": 1,
+									"num": params["n_obs"],
+									"order": "random"
+								}
+					]
+			},
+			"simulation_parameters": {
+					"debug": params["debug"],
+					"master_seed": params["seed"],
+					"n_sims": params["n_sims"],
+					"output_path": params["output_path"],
+					"output_prefix": "",
+					"progress": params["progress"],
+					"verbose": params["verbose"],
+					"save_pubs": params["save_pubs"],
+					"save_sims": params["save_sims"],
+					"save_stats": params["save_stats"],
+					"save_rejected": params["save_rejected"]
+			}
+		}
+
+		uid = str(uuid.uuid4())
+		filename = uid + ".json" 
+		configfilenames.write(filename + "\n")
+
+		# Replacing the output prefix with a unique id
+		data["simulation_parameters"]["output_prefix"] = uid
+
+		with open("configs/" + filename, 'w') as f:
+				json.dump(data, f, indent = 4)
+
+	print(" %d configuration files have generated!" % counter)
+
+if __name__ == '__main__':
+	main()
