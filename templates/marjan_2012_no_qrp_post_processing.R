fe14657198ae2973fd5cde56d9efc0e079064854
Amir Masoud Abdol <amirmasoudabdol@icloud.com>
2020-09-27 11:13:39 +0200

    removed n_reps bug

--- templates/marjan_2012_no_qrp_post_processing.R
+++ templates/marjan_2012_no_qrp_post_processing.R
@@ -44,10 +44,9 @@ summarize_each_file <- function(fname) {
            is_hacked = factor(researcher_parameters_probability_of_being_a_hacker),
            tmean = experiment_parameters_data_strategy_measurements_means_2,
            alpha = experiment_parameters_test_strategy_alpha,
-           n_reps = experiment_parameters_n_reps,
            test_strategy = experiment_parameters_test_strategy_name,
            n_pos_sig = if_else(effect > 0 & sig, 1, 0)) %>%
-    group_by(tmean, tnobs, alpha, n_reps, sizeclass, decision_strategy, test_strategy, is_hacked) %>%
+    group_by(tmean, tnobs, alpha, sizeclass, decision_strategy, test_strategy, is_hacked) %>%
     summarize(sigmean = sum(n_pos_sig) / n(),
               mean_nobs = mean(nobs),
               n_reps = head(n_reps),
