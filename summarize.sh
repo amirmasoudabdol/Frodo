043d44d22c5769a4761018ac052fad23224bc8ed
Amir Masoud Abdol <amirmasoudabdol@icloud.com>
2021-09-17 13:36:45 +0200

    moving some files around

--- summarize.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH -N 1
-#SBATCH -n 16
-#SBATCH -t 00:15:00
-#SBATCH --mail-type=BEGIN,END
-#SBATCH --mail-user=a.m.abdol@uvt.nl
-
-make csv from=EggersTestEstimator_Summaries; make stack from=EggersTestEstimator_Summaries; make csv from=RandomEffectEstimator_Summaries; make stack from=RandomEffectEstimator_Summaries; make csv from=Publications_Summaries; make stack from=Publications_Summaries
